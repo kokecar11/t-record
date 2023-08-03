9dace1a67dd319f7a389b5c3d0def650d067610c
Samuel Llibre Santos <kuzenshi@gmail.com>
2023-08-03 00:15:47 -0300

    fix(component): 🐛 fix bug with transition on collapse, add group hover effect, organize code

--- src/components/collapse/Collapse.tsx
+++ src/components/collapse/Collapse.tsx
@@ -1,30 +1,49 @@
-import { component$, useTask$ } from "@builder.io/qwik";
-import { Icon, IconCatalog } from "../icon/icon";
-import { Link } from "@builder.io/qwik-city";
-import { useCollapse } from "./hooks/use-collapse";
+import { component$, useTask$ } from '@builder.io/qwik'
+import { Icon, IconCatalog } from '../icon/icon'
+import { Link } from '@builder.io/qwik-city'
+import { useCollapse } from './hooks/use-collapse'
+import cn from 'classnames'
 
 export interface CollapseProps {
-    title: string
-    description: string
-    isOpen?: boolean
+  title: string
+  description: string
+  isOpen?: boolean
 }
 
-export const Collapse = component$(({title, description, isOpen}:CollapseProps) => {
-    const {isOpenCollapse, showCollapse} = useCollapse();
+export const Collapse = component$(
+  ({ title, description, isOpen }: CollapseProps) => {
+    const { isOpenCollapse, showCollapse } = useCollapse()
     useTask$(() => {
-        if(isOpen) isOpenCollapse.value = true;
+      if (isOpen) isOpenCollapse.value = true
     })
+
+    const classes = {
+      content: cn('mt-1 transition-all overflow-hidden max-h-fit', {
+        'h-36': isOpenCollapse.value,
+        'h-0': !isOpenCollapse.value,
+      }),
+    }
+
     return (
-        <div class="cursor-pointer border border-secondary border-opacity-30 bg-accent max-w-full p-4 rounded-lg hover:bg-opacity-50 focus:bg-opacity-50" onClick$={showCollapse}>
-            <div class="text-left text-2xl font-bold flex">
-                <span class="flex-1">{title}</span>
-                <Link class="flex-none rounded-lg hover:bg-primary p-1 cursor-pointer transition duration-150 ease-in-out">
-                    <Icon name={IconCatalog.feArrowDown} class={`flex-none ${isOpenCollapse.value ? 'rotate-180 transition-all animate-duration-300 delay-75': 'rotate-0 transition-all animate-duration-300 delay-75'}`} />
-                </Link>
-            </div>
-            <div class={`${isOpenCollapse.value ? 'block':'hidden'} mt-1 p-3 text-left`}>
-                <p class="transition-all animate-duration-500 animate-fade-down">{description}</p>
-            </div>
+      <div
+        class="cursor-pointer border group border-secondary border-opacity-30bg-accent p-4 rounded-lg hover:bg-opacity-50 focus:bg-opacity-50"
+        onClick$={showCollapse}
+      >
+        <div class="text-left text-2xl font-bold flex items-start justify-between">
+          <span>{title}</span>
+          <Link class="rounded-lg p-1 cursor-pointer transition duration-150 ease-in-out group-hover:bg-primary">
+            <Icon
+              name={IconCatalog.feArrowDown}
+              class={` animate-duration-300  delay-75 transition-transform ${
+                isOpenCollapse.value ? '-rotate-180' : 'rotate-0 '
+              }`}
+            />
+          </Link>
         </div>
-    );
-});
+        <div class={classes.content}>
+          <p>{description}</p>
+        </div>
+      </div>
+    )
+  },
+)
