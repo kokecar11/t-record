849bcb748dfaf4082f4be0156f0aebca49c4c22a
Koke Carpintero <kokecar11.dev@gmail.com>
2023-08-03 21:23:33 -0500

    feat(config): add GTagManager func in root

--- src/root.tsx
+++ src/root.tsx
@@ -6,6 +6,7 @@ import { AuthSessionContext } from './auth/context/auth.context';
 import { GlobalStore, type SiteStore } from './core/context';
 import { type Live, LiveStreamContext } from './live/context/live.context';
 import './global.css';
+import { QwikPartytown } from './components/partytown/partytown';
 
 export default component$(() => {
   /**
@@ -33,6 +34,16 @@ export default component$(() => {
         <meta charSet="utf-8" />
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
+        <QwikPartytown />
+
+        <script type="text/partytown"  dangerouslySetInnerHTML={`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MLT5XFTT');
+        `} ></script>
+
       </head>
       <body lang="en">
         <RouterOutlet />
