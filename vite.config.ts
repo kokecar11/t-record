ae334e7634460a2f7cb2380369ec809ababefff3
Koke Carpintero <kokecar11.dev@gmail.com>
2023-12-12 01:53:35 -0500

    [TR-16] fix: prisma not building vercel

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig(() => {
     },
     resolve: {
       alias: {
-        ".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
+        "@prisma/client/index-browser": "./node_modules/@prisma/client/index-browser.js"
       }
     },
   };
